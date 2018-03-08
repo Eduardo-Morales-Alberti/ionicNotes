adc41bd30442ce9f72ef4f5021fd6e79d64b7548
Eduardo <eduardo.morales@metadrop.net>
2018-03-08 16:04:24 +0100

    Initial Commit

---
+++ src/pages/test/test.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+/**
+ * Generated class for the TestPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-test',
+  templateUrl: 'test.html',
+})
+export class TestPage {
+
+  constructor() {
+  }
+
+}
